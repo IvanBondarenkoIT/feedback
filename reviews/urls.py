17b60d6f32fec16d683ca4cc3a230cee81cbfdaa
Ivan <ivan.bondarenko.it@gmail.com>
2023-03-29 22:10:06 +0400

    29.03 Forms: CSRF Protection

--- reviews/urls.py
+++ reviews/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     # path('admin/', admin.site.urls),
     # path('review/', views.review),
     path('', review),
+    path('thank-you', views.thank_you),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -3,4 +3,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def review(request):
+    if request.method == 'POST':
+        entered_username = request.POST['username']
+        print(entered_username)
+        return render(request,'reviews/thank_you.html')
+
     return render(request, 'reviews/review.html')
+
+
+def thank_you(request):
+    pass
