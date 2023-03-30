836ccb4fd283d1f3119ade7d2918d3e91903303d
Ivan <ivan.bondarenko.it@gmail.com>
2023-03-30 11:57:13 +0400

    30.03 Forms: Storing form data in database

--- reviews/forms.py
+++ reviews/forms.py
@@ -5,5 +5,6 @@ class ReviewForm(forms.Form):
     user_name = forms.CharField(label="Your name", max_length=100, error_messages={
         "required": "Your name is required",
         "max_length": "Please enter shorter name!",
-
     })
+    review_text = forms.CharField(label="Your feedback", max_length=200, widget=forms.Textarea)
+    rating = forms.IntegerField(label="Your rating", min_value=1, max_value=5)
