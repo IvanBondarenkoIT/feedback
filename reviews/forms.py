81b77197d2320fcd27a2e33021583770625d51a0
Ivan <ivan.bondarenko.it@gmail.com>
2023-03-30 11:15:38 +0400

    30.03 Forms: Stiling Forms

--- reviews/forms.py
+++ reviews/forms.py
@@ -2,4 +2,8 @@ from django import forms
 
 
 class ReviewForm(forms.Form):
-    user_name = forms.CharField()
+    user_name = forms.CharField(label="Your name", max_length=100, error_messages={
+        "required": "Your name is required",
+        "max_length": "Please enter shorter name!",
+
+    })
